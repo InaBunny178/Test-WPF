2664523cadfe23773b83700cbe2c572bb600f177
InaBunny178 <kkdevelop178@gmail.com>
2013-12-21 15:42:23 +0900

    Button追加

--- TestGit/MainWindow.xaml.cs
+++ TestGit/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TestGit
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Button Clicked.");
+        }
     }
 }
